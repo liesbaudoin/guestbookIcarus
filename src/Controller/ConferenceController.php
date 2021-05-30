3503ce46251e94f793ce4a70899c487a17558fc1
Liesbeth Baudoin <liesbaudoin@hotmail.com>
2021-05-30 13:41:43 +0200

    paginating the comments

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Comment;
 use App\Entity\Conference;
 use App\Repository\ConferenceRepository;
 use App\Repository\CommentRepository;
@@ -22,13 +23,23 @@ class ConferenceController extends AbstractController
             'conferences' => $conferenceRepository->findAll(),
         ]));
     }
+
 #[Route('/conference/{id}', name: 'conference')]
-    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository):Response
+
+
+    public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
     {
-return new Response($twig->render('conference/show.html.twig', [
-    'conference'=> $conference,
-    'comments'=>$commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC'])
-]));
+        $offset = max(0, $request->query->getInt('offset', 0));
+        /**
+         * @var  Comment[] het is een object waarover je kan loopen, het resultaat van je query zit hierin
+         */
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+        ]));
     }
 
 }
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Comment;
+use App\Entity\Conference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -14,9 +16,28 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentRepository extends ServiceEntityRepository
 {
+    public const PAGINATOR_PER_PAGE =2;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
+    }
+    public function getCommentPaginator(Conference $conference, int $offset) : Paginator{
+        //Querybuilder doet altijd dit voor de repository waar je in zit: select * from comments (dus omdat je in deze repository zit)
+        $query = $this->createQueryBuilder('c')
+            //where conference = hier eerst als placeholder
+        ->andWhere('c.conference = :conference')
+            //hier bind je de parameter door de $conference die meegegeven is als argument aan deze function
+            ->setParameter('conference', $conference)
+            //nu hebben we select * from comments where conference = $conference je wil een sortereing hebben, dus orderBy
+            ->orderBy('c.createdAt', 'DESC')
+
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            //begin bij (eerst bij 0 dan bij 2 etc
+            ->setFirstResult($offset)
+            ->getQuery();
+        return new Paginator($query);
+
     }
 
     // /**
